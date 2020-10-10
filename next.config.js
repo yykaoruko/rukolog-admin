a4c12b75541d69f38d74eaad1058f574c0dc651d
Kaoruko <yykaoruko@gmail.com>
2020-10-10 19:04:57 +0900

    feat: set alias

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const env = require(`./src/config/${process.env.NODE_ENV}.js`);
+const path = require('path');
 
 module.exports = {
   env: env,
+  webpack: config => {
+    config.resolve.alias['~'] = path.resolve(__dirname);
+    return config;
+  }
 }
